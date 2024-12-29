e73eaa5b6643cfb0dd69d573e14f81a475d57570
Pedro Freitas <pedro.hnd.freitas@gmail.com>
2024-12-29 12:45:50 -0300

    [feature] - creating book service

---
+++ book-service/src/main/java/dev/phdfreitas/bookservice/model/Book.java
@@ -0,0 +1,112 @@
+package dev.phdfreitas.bookservice.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+public class Book implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "book_seq")
+    @SequenceGenerator(name = "book_seq", sequenceName = "book_sequence", allocationSize = 1)
+    private Long id;
+
+    private String author;
+
+    @Temporal(TemporalType.DATE)
+    private Date launchDate;
+    private BigDecimal price;
+    private String title;
+
+    @Transient
+    private String currency;
+    @Transient
+    private String enviroment;
+
+    public Book() {}
+
+    public Book(Long id, String author, Date launchDate, BigDecimal price, String title, String currency, String enviroment) {
+        this.id = id;
+        this.author = author;
+        this.launchDate = launchDate;
+        this.price = price;
+        this.title = title;
+        this.currency = currency;
+        this.enviroment = enviroment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getLaunchDate() {
+        return launchDate;
+    }
+
+    public void setLaunchDate(Date launchDate) {
+        this.launchDate = launchDate;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getEnviroment() {
+        return enviroment;
+    }
+
+    public void setEnviroment(String enviroment) {
+        this.enviroment = enviroment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ book-service/src/main/resources/db/migration/V1__Create_Table_Books.sql
@@ -0,0 +1,9 @@
+﻿CREATE SEQUENCE book_sequence START 1;
+
+CREATE TABLE book (
+    id BIGINT PRIMARY KEY DEFAULT nextval('book_sequence'),
+    author TEXT,
+    launch_date TIMESTAMP NOT NULL,
+    price NUMERIC(65, 2) NOT NULL,
+    title TEXT
+);
