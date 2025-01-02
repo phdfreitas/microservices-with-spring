274b8af8230b4e27307d7eb596b846bb1ae58dfc
Pedro Freitas <pedro.hnd.freitas@gmail.com>
2025-01-02 07:47:44 -0300

    [feature] - usign rest template to consuming exchange-service

--- book-service/src/main/java/dev/phdfreitas/bookservice/controller/BookController.java
+++ book-service/src/main/java/dev/phdfreitas/bookservice/controller/BookController.java
@@ -2,12 +2,16 @@ package dev.phdfreitas.bookservice.controller;
 
 import dev.phdfreitas.bookservice.model.Book;
 import dev.phdfreitas.bookservice.repository.BookRepository;
+import dev.phdfreitas.bookservice.response.Exchange;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("book-service")
@@ -26,8 +30,20 @@ public class BookController {
         if (book == null)
             throw new RuntimeException("Book not found");
 
+        HashMap<String, String> params = new HashMap<>();
+        params.put("amount", book.get().getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+
+        var response = new RestTemplate().getForEntity("http://localhost:8000/exchange-service/{amount}/{from}/{to}",
+                Exchange.class, params);
+
+        var exchange = response.getBody();
+
         var port = environment.getProperty("local.server.port");
         book.get().setEnviroment(port);
+        book.get().setCurrency(currency);
+        book.get().setPrice(exchange.getConvertedValue());
 
         return book.get();
     }
